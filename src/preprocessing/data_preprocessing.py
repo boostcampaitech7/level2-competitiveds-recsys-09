0ad78c2365ca7d749fd37cabc14b53bc80221657
hansg931 <hansg0509@gmail.com>
2024-10-16 17:48:17 +0900

    Feat: `interest_rate` 병합

--- src/preprocessing/data_preprocessing.py
+++ src/preprocessing/data_preprocessing.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from src.preprocessing.remove_data import remove_duplicates, latlng_boundary_filter
 from src.preprocessing.split_data import split_year_month
 
@@ -5,6 +7,13 @@ from src.preprocessing.split_data import split_year_month
 def data_preprocessing(train_data, test_data, interest_rate, subway_info, school_info, park_info):
 	train_data = remove_duplicates(train_data)
 	test_data = remove_duplicates(test_data)
+
+	#  Merge interest rate data
+	train_data = pd.merge(train_data, interest_rate, left_on='contract_year_month', right_on='year_month', how='left')
+	train_data.drop(columns=['year_month'], inplace=True)
+
+	test_data = pd.merge(test_data, interest_rate, left_on='contract_year_month', right_on='year_month', how='left')
+	test_data.drop(columns=['year_month'], inplace=True)
 
 	train_data = split_year_month(train_data)
 	test_data = split_year_month(test_data)

8c70dbac7482de9785d7a618a2f9740c8b4e73bb
hansg931 <hansg0509@gmail.com>
2024-10-16 18:18:13 +0900

    Feat: feature 추가 - (`log_transformed`) `area_m2`, `floor`, `age` - K-Means clustering - `interest_rate_3mo_avg` <- `interest_rate` 3 month rolling average

--- src/features/feature_engineering.py
+++ src/features/feature_engineering.py
@@ -1,3 +1,6 @@
+import numpy as np
+from sklearn.cluster import KMeans
+
 from src.features.nearest_public import calculate_nearst_distances, transform_distances
 
 
@@ -10,10 +13,44 @@ def feature_engineering(train_data, test_data, interest_rate, subway_info, schoo
 	train_data = transform_distances(train_data, distance_columns)
 	test_data = transform_distances(test_data, distance_columns)
 
-	columns_needed = ['area_m2', 'contract_year_month', 'contract_type', 'floor', 'latitude', 'longitude', 'deposit', 'built_year']
-	columns_needed_test = ['area_m2', 'contract_year_month', 'contract_type', 'floor', 'latitude', 'longitude', 'built_year']
+	# TODO: Modularize
+	###
+	train_area_m2_min = train_data[train_data['area_m2'] > 0]['area_m2'].min()
+	test_area_m2_min = test_data[test_data['area_m2'] > 0]['area_m2'].min()
+	train_data['log_area_m2'] = np.where(train_data['area_m2'] > 0, np.log1p(train_data['area_m2']), train_area_m2_min)
+	test_data['log_area_m2'] = np.where(test_data['area_m2'] > 0, np.log1p(test_data['area_m2']), test_area_m2_min)
 
-	# train_data = train_data[columns_needed]
-	# test_data = test_data[columns_needed_test]
+	train_floor_min = train_data[train_data['floor'] > 0]['floor'].min()
+	test_floor_min = test_data[test_data['floor'] > 0]['floor'].min()
+	train_data['log_floor'] = np.where(train_data['floor'] > 0, np.log1p(train_data['floor']), train_floor_min)
+	test_data['log_floor'] = np.where(test_data['floor'] > 0, np.log1p(test_data['floor']), test_floor_min)
+
+	train_age_min = train_data[train_data['age'] > 0]['age'].min()
+	test_age_min = test_data[test_data['age'] > 0]['age'].min()
+	train_data['log_age'] = np.where(train_data['age'] > 0, np.log1p(train_data['age']), train_age_min)
+	test_data['log_age'] = np.where(test_data['age'] > 0, np.log1p(test_data['age']), test_age_min)
+
+	train_data = train_data.drop(columns=['area_m2', 'floor', 'age'])
+	test_data = test_data.drop(columns=['area_m2', 'floor', 'age'])
+
+	# K-Means Clustering
+	kmeans = KMeans(n_clusters=100, random_state=42)
+	train_data['region_cluster'] = kmeans.fit_predict(train_data[['latitude', 'longitude']])
+	test_data['region_cluster'] = kmeans.predict(test_data[['latitude', 'longitude']])
+
+	# Rolling Average
+	train_data['interest_rate_3mo_avg'] = train_data['interest_rate'].rolling(window=3).mean()
+	test_data['interest_rate_3mo_avg'] = test_data['interest_rate'].rolling(window=3).mean()
+	###
+
+	columns_needed = ['deposit', 'log_area_m2', 'year', 'month', 'log_floor', 'latitude', 'longitude',
+					'log_subway_distance', 'log_school_distance', 'log_park_distance', 'log_age', 'region_cluster',
+					'interest_rate_3mo_avg']
+	columns_needed_test = ['log_area_m2', 'year', 'month', 'log_floor', 'latitude', 'longitude',
+						'log_subway_distance', 'log_school_distance', 'log_park_distance', 'log_age',
+						'region_cluster', 'interest_rate_3mo_avg']
+
+	train_data = train_data[columns_needed]
+	test_data = test_data[columns_needed_test]
 
 	return train_data, test_data

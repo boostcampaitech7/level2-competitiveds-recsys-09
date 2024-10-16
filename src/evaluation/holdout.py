83bd762d4265ff4d9ecbccb4c8f46c977b77984f
hansg931 <hansg0509@gmail.com>
2024-10-16 15:43:49 +0900

    Feat: `get_holdout_data` 반환값 수정

--- src/evaluation/holdout.py
+++ src/evaluation/holdout.py
@@ -1,9 +1,16 @@
+from pandas import DataFrame
+
 from src.utils.variables import HOLDOUT_START, HOLDOUT_END
 
 
-def get_holdout_data(train_data):
+def get_holdout_data(train_data: DataFrame) -> (DataFrame, DataFrame):
+	"""
+	Get holdout data from train data
+	:param train_data: (DataFrame) Train data
+	:return: (DataFrame, DataFrame) Holdout data, Train data
+	"""
 	holdout_data = train_data[
 		(train_data['contract_year_month'] >= HOLDOUT_START) & (train_data['contract_year_month'] <= HOLDOUT_END)]
 	train_data = train_data[
 		~((train_data['contract_year_month'] >= HOLDOUT_START) & (train_data['contract_year_month'] <= HOLDOUT_END))]
-	return holdout_data
+	return holdout_data, train_data

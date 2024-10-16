4b6d947be168e91a8e69bdeccf47d58a74ed706c
hansg931 <hansg0509@gmail.com>
2024-10-16 15:40:23 +0900

    Feat: `info` latlng 범위 설정

--- src/preprocessing/data_preprocessing.py
+++ src/preprocessing/data_preprocessing.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+from src.utils.variables import MIN_LAT, MAX_LAT, MIN_LNG, MAX_LNG
+
 
 def remove_duplicates(data: pd.DataFrame) -> pd.DataFrame:
 	"""
@@ -8,3 +10,8 @@ def remove_duplicates(data: pd.DataFrame) -> pd.DataFrame:
 	:return: (pd.DataFrame) DataFrame without duplicates
 	"""
 	return data[~data.drop(columns=['index']).duplicated()].drop(columns=['index'])
+
+
+def set_latlng_boundary(data: pd.DataFrame) -> pd.DataFrame:
+	return data[(data['latitude'] >= MIN_LAT) & (data['latitude'] <= MAX_LAT) & (data['longitude'] >= MIN_LNG) & (
+			data['longitude'] <= MAX_LNG)]

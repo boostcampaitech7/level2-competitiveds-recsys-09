f7b50fc556017479fecc83fe8518adb9d8348439
hansg931 <hansg0509@gmail.com>
2024-10-16 15:45:28 +0900

    Refactor: `pd.DataFrame` -> `DataFrame`

--- src/preprocessing/data_preprocessing.py
+++ src/preprocessing/data_preprocessing.py
@@ -1,17 +1,17 @@
-import pandas as pd
+from pandas import DataFrame
 
 from src.utils.variables import MIN_LAT, MAX_LAT, MIN_LNG, MAX_LNG
 
 
-def remove_duplicates(data: pd.DataFrame) -> pd.DataFrame:
+def remove_duplicates(data: DataFrame) -> DataFrame:
 	"""
 	Remove duplicates except index from the DataFrame
-	:param data: (pd.DataFrame) DataFrame to remove duplicates
-	:return: (pd.DataFrame) DataFrame without duplicates
+	:param data: (DataFrame) DataFrame to remove duplicates
+	:return: (DataFrame) DataFrame without duplicates
 	"""
 	return data[~data.drop(columns=['index']).duplicated()].drop(columns=['index'])
 
 
-def set_latlng_boundary(data: pd.DataFrame) -> pd.DataFrame:
+def set_latlng_boundary(data: DataFrame) -> DataFrame:
 	return data[(data['latitude'] >= MIN_LAT) & (data['latitude'] <= MAX_LAT) & (data['longitude'] >= MIN_LNG) & (
 			data['longitude'] <= MAX_LNG)]

24563cd104801298fe60b49eefda2bd067550576
hansg931 <hansg0509@gmail.com>
2024-10-17 01:21:29 +0900

    Feat: `XGBoost` 모델 구현 - `optuna` 적용

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -2,8 +2,10 @@ import lightgbm as lgb
 from numpy import ndarray
 from pandas import DataFrame
 import time
+import xgboost as xgb
 
 from src.models.lgb import optimize_lgb
+from src.models.xgb import optimize_xgb
 
 
 def train_model(X_train: DataFrame, y_train: DataFrame, X_holdout: DataFrame, y_holdout: DataFrame, X_test: DataFrame) -> ndarray:
@@ -22,12 +24,22 @@ def train_model(X_train: DataFrame, y_train: DataFrame, X_holdout: DataFrame, y_
 
 	start_time = time.time()
 
-	best_params = optimize_lgb(X_train, y_train, X_holdout, y_holdout, 1)
+	# LightGBM
+	###
+	# best_params = optimize_lgb(X_train, y_train, X_holdout, y_holdout, 1)
+	#
+	# final_lgb = lgb.LGBMRegressor(**best_params)
+	# final_lgb.fit(X_train, y_train)
+	#
+	# y_test_pred = final_lgb.predict(X_test)
+	###
 
-	final_lgb = lgb.LGBMRegressor(**best_params)
-	final_lgb.fit(X_train, y_train)
+	best_params = optimize_xgb(X_train, y_train, X_holdout, y_holdout, 1)
 
-	y_test_pred = final_lgb.predict(X_test)
+	final_xgb = xgb.XGBRegressor(**best_params)
+	final_xgb.fit(X_train, y_train)
+
+	y_test_pred = final_xgb.predict(X_test)
 
 	print(f"Model Training took {time.time() - start_time:.2f} seconds")
 
--- src/models/xgb.py
+++ src/models/xgb.py
@@ -1,8 +1,62 @@
-from src.utils.variables import RANDOM_SEED
-from xgboost import XGBRegressor
+import xgboost as xgb
+import optuna
+from pandas import DataFrame
+from sklearn.metrics import mean_absolute_error
 
-def train_xgb(X_train, y_train):
-	xgb_model = XGBRegressor(random_state=RANDOM_SEED)
+from src.utils.variables import RANDOM_SEED
+
+
+def optimize_xgb(X_train: DataFrame, y_train: DataFrame, X_holdout: DataFrame, y_holdout: DataFrame,
+				 n_trials=100) -> dict:
+	"""
+	Optimize XGBoost hyperparameters using Optuna
+	:param X_train: (DataFrame) Feature data for training
+	:param y_train: (DataFrame) Target data for training
+	:param X_holdout: (DataFrame) Feature data for holdout
+	:param y_holdout: (DataFrame) Target data for holdout
+	:param n_trials: (int) Number of trials, default=100
+	:return: (dict) Best hyperparameters
+	"""
+	print('==============================')
+	print('XGBoost Optimization')
+	print('==============================')
+
+	study = optuna.create_study(direction='minimize')
+	study.optimize(lambda trial: objective(trial, X_train, y_train, X_holdout, y_holdout), n_trials=n_trials)
+
+	print(f'Best trial: {study.best_trial.value}')
+	print(f'Best params: {study.best_params}')
+
+	return study.best_params
+
+
+def objective(trial, X_train: DataFrame, y_train: DataFrame, X_holdout: DataFrame, y_holdout: DataFrame) -> float:
+	"""
+	Optuna objective function for XGBoost
+	:param trial: (optuna.trial.Trial) A trial is a process of evaluating an objective function
+	:param X_train: (DataFrame) Feature data for training
+	:param y_train: (DataFrame) Target data for training
+	:param X_holdout: (DataFrame) Feature data for holdout
+	:param y_holdout: (DataFrame) Target data for holdout
+	:return: (float) MAE score
+	"""
+	params = {
+		'objective': 'reg:squarederror',
+		'eval_metric': 'mae',
+		'seed': RANDOM_SEED,
+		'learning_rate': trial.suggest_float('learning_rate', 1e-3, 1e-1),
+		'n_estimators': trial.suggest_int('n_estimators', 50, 500),
+		'max_depth': trial.suggest_int('max_depth', 3, 20),
+		'min_child_weight': trial.suggest_int('min_child_weight', 1, 10),
+		'gamma': trial.suggest_float('gamma', 0, 5),
+		'subsample': trial.suggest_float('subsample', 0.1, 1.0),
+		'colsample_bytree': trial.suggest_float('colsample_bytree', 0.1, 1.0),
+	}
+
+	xgb_model = xgb.XGBRegressor(**params)
 	xgb_model.fit(X_train, y_train)
 
-	return xgb_model
+	y_pred = xgb_model.predict(X_holdout)
+	mae = mean_absolute_error(y_holdout, y_pred)
+
+	return mae

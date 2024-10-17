8388a65ad7f1bd4ba7bc6c0b6dee6acdcf01e357
hansg931 <hansg0509@gmail.com>
2024-10-17 16:18:56 +0900

    Feat: 데이터 설치 경로 수정

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -15,7 +15,7 @@ def download_data():
 def extract_data():
 	print('Extracting Data')
 
-	os.system('tar -xvf ./data.tar.gz -C ./data/raw/')
+	os.system('tar -xvf ./data.tar.gz --strip-components=1 -C ./data/raw/')
 	os.system('rm ./data.tar.gz')
 
 	print('Data Extracted')

93d62f4af02f21041c2dc5db7986598e35e4ba67
hhj <mnbdj12345@gmail.com>
2024-10-21 01:39:22 +0900

    Feat: 새롭게 계산한 feature ./dara/preprocessed에 저장

--- src/features/cluster_feature_add.py
+++ src/features/cluster_feature_add.py
@@ -1,47 +1,39 @@
 import pandas as pd
+import os
 from src.features.cluster_deposit_per_area import calculate_cluster_deposit_per_m2, add_cluster_results_to_train_data, assign_clusters_to_test_data, add_test_data_clusters_and_deposit
 from src.features.clustering import kmeans
 
 def perform_clustering_and_calculate_deposit(train_data, test_data, n_clusters_list, max_iters, tol):
-    """
-    Perform K-Means clustering and calculate cluster deposit per m².
-    Return new features without modifying original train/test data.
-    
-    :param train_data: (pd.DataFrame) Training data
-    :param test_data: (pd.DataFrame) Test data
-    :param n_clusters_list: (list) List of numbers of clusters to use
-    :param max_iters: (int) Maximum iterations for K-Means
-    :param tol: (float) Convergence tolerance
-    :return: (list of pd.DataFrames, list of pd.DataFrames) Cluster features for train_data and test_data
-    """
-    train_features = []
-    test_features = []
+    if os.path.exists('./data/preprocessed/train_cluster_features.csv') and os.path.exists('./data/preprocessed/test_cluster_features.csv'):
+        print("이미 처리된 파일이 존재합니다. cluster 연산 건너뜁니다.")
+        return pd.read_csv('./data/preprocessed/train_cluster_features.csv'), pd.read_csv('./data/preprocessed/test_cluster_features.csv')
+
+
+    train_features = [train_data[['latitude', 'longitude']]]
+    test_features = [test_data[['latitude', 'longitude']]]
     
     for n_clusters in n_clusters_list:
-        # Perform K-Means clustering
         centroids, cluster_assignments = kmeans(train_data, n_clusters, max_iters, tol)
-        
-        # Calculate deposit per m² for each cluster in train_data
+
         cluster_deposit_per_m2 = calculate_cluster_deposit_per_m2(train_data, cluster_assignments, n_clusters)
-        
-        # Create clustering result features for train_data
         train_cluster_features = add_cluster_results_to_train_data(train_data[['latitude', 'longitude']], 
                                                                    cluster_assignments, 
                                                                    cluster_deposit_per_m2, 
                                                                    str(n_clusters))
-        train_features.append(train_cluster_features)
+        train_features.append(train_cluster_features.drop(columns=['latitude', 'longitude']))
         
-        # Assign clusters to test_data
         cluster_assignments_test = assign_clusters_to_test_data(test_data, centroids)
         
-        # Create clustering result features for test_data
         test_cluster_features = add_test_data_clusters_and_deposit(test_data[['latitude', 'longitude']], 
                                                                    cluster_assignments_test, 
                                                                    cluster_deposit_per_m2, 
                                                                    str(n_clusters))
-        test_features.append(test_cluster_features)
+        test_features.append(test_cluster_features.drop(columns=['latitude', 'longitude']))
 
+    # DataFrame 병합
     train_features_df = pd.concat(train_features, axis=1)
     test_features_df = pd.concat(test_features, axis=1)
     
+    train_features_df.to_csv('./data/preprocessed/train_cluster_features.csv', index=False)
+    test_features_df.to_csv('./data/preprocessed/test_cluster_features.csv', index=False)
     return train_features_df, test_features_df

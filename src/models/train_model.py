962366af17090997b77dd61147f4cb3fcdb4d72a
hyeonjinha <mnbdj12345@gmail.com>
2024-10-21 05:06:06 +0000

    Refac: train_model.py 원위치

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -3,57 +3,50 @@ from numpy import ndarray
 from pandas import DataFrame
 import time
 import xgboost as xgb
-import torch
 
 from src.models.lgb import optimize_lgb
 from src.models.xgb import optimize_xgb
 
 
 def train_model(X_train: DataFrame, y_train: DataFrame, X_holdout: DataFrame, y_holdout: DataFrame, X_test: DataFrame,
-                model: str, n_trials: int, n_jobs: int) -> ndarray:
-    """
-    Train Models
-    :param X_train: (DataFrame) Feature data for training
-    :param y_train: (DataFrame) Target data for training
-    :param X_holdout: (DataFrame) Feature data for holdout
-    :param y_holdout: (DataFrame) Target data for holdout
-    :param X_test: (DataFrame) Feature data for testing
-    :param model: (str) Model to train
-    :param n_trials: (int) Number of trials
-    :param n_jobs: (int) Number of CPU threads
-    :return:
-    """
-    print("==============================")
-    print('Training model')
-    print("==============================")
+				model: str, n_trials: int, n_jobs: int) -> ndarray:
+	"""
+	Train Models
+	:param X_train: (DataFrame) Feature data for training
+	:param y_train: (DataFrame) Target data for training
+	:param X_holdout: (DataFrame) Feature data for holdout
+	:param y_holdout: (DataFrame) Target data for holdout
+	:param X_test: (DataFrame) Feature data for testing
+	:param model: (str) Model to train
+	:param n_trials: (int) Number of trials
+	:param n_jobs: (int) Number of CPU threads
+	:return:
+	"""
+	print("==============================")
+	print('Training model')
+	print("==============================")
 
-    start_time = time.time()
+	start_time = time.time()
 
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+	if model == 'lgb':
+		best_params = optimize_lgb(X_train, y_train, X_holdout, y_holdout, n_trials=n_trials, n_jobs=n_jobs)
+		final_model = lgb.LGBMRegressor(**best_params, n_jobs=n_jobs)
+		final_model.fit(X_train, y_train)
 
-    if model == 'lgb':
-        best_params = optimize_lgb(X_train, y_train, X_holdout, y_holdout, n_trials=n_trials, n_jobs=n_jobs)
-        if device == 'cuda':
-            best_params['device'] = 'gpu'
-        final_model = lgb.LGBMRegressor(**best_params, n_jobs=n_jobs)
-        final_model.fit(X_train, y_train)
+	elif model == 'xgb':
+		best_params = optimize_xgb(X_train, y_train, X_holdout, y_holdout, n_trials=n_trials, n_jobs=n_jobs)
+		final_model = xgb.XGBRegressor(**best_params, n_jobs=n_jobs)
+		final_model.fit(X_train, y_train)
 
-    elif model == 'xgb':
-        best_params = optimize_xgb(X_train, y_train, X_holdout, y_holdout, n_trials=n_trials, n_jobs=n_jobs)
-        if device == 'cuda':
-            best_params['tree_method'] = 'gpu_hist'
-        final_model = xgb.XGBRegressor(**best_params, n_jobs=n_jobs)
-        final_model.fit(X_train, y_train)
+	else:
+		raise ValueError(f"Unsupported model type: {model}")
 
-    else:
-        raise ValueError(f"Unsupported model type: {model}")
+	y_test_pred = final_model.predict(X_test)
 
-    y_test_pred = final_model.predict(X_test)
+	print(f"Model Training took {time.time() - start_time:.2f} seconds")
 
-    print(f"Model Training took {time.time() - start_time:.2f} seconds")
+	print("==============================")
+	print(f'{model.upper()} Model trained')
+	print("==============================")
 
-    print("==============================")
-    print(f'{model.upper()} Model trained')
-    print("==============================")
-
-    return y_test_pred
+	return y_test_pred

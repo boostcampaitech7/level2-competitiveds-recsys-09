fb827a7c5c824891712ba1cb5ce75dfd9d608b8a
imnoans <sy1004da@gmail.com>
2024-10-28 08:32:57 +0900

    end_project

--- src/features/feature_engineering.py
+++ src/features/feature_engineering.py
@@ -17,52 +17,7 @@ def feature_engineering(train_data, test_data, interest_rate, subway_info, eleme
 
 	start_time = time.time()
 
-	facilities_info = {'subway': subway_info, 'elementary': elementary_info, 'middle': middle_info, 'high': high_info, 'park': park_info}
-	print('start train_data distance calculation')
-	train_data = calculate_nearst_distances_count_id(train_data, facilities_info)
-	print('start test_data distance calculation')
-	test_data = calculate_nearst_distances_count_id(test_data, facilities_info)
-
-	'''distance_columns = ['subway_distance', 'school_distance', 'park_distance']
-	train_data = transform_distances(train_data, distance_columns)
-	test_data = transform_distances(test_data, distance_columns)'''
-
-	# TODO: Modularize
-	###
-	'''train_area_m2_min = train_data[train_data['area_m2'] > 0]['area_m2'].min()
-	test_area_m2_min = test_data[test_data['area_m2'] > 0]['area_m2'].min()
-	train_data['log_area_m2'] = np.where(train_data['area_m2'] > 0, np.log1p(train_data['area_m2']), train_area_m2_min)
-	test_data['log_area_m2'] = np.where(test_data['area_m2'] > 0, np.log1p(test_data['area_m2']), test_area_m2_min)
-
-	train_floor_min = train_data[train_data['floor'] > 0]['floor'].min()
-	test_floor_min = test_data[test_data['floor'] > 0]['floor'].min()
-	train_data['log_floor'] = np.where(train_data['floor'] > 0, np.log1p(train_data['floor']), train_floor_min)
-	test_data['log_floor'] = np.where(test_data['floor'] > 0, np.log1p(test_data['floor']), test_floor_min)
-
-	train_age_min = train_data[train_data['age'] > 0]['age'].min()
-	test_age_min = test_data[test_data['age'] > 0]['age'].min()
-	train_data['log_age'] = np.where(train_data['age'] > 0, np.log1p(train_data['age']), train_age_min)
-	test_data['log_age'] = np.where(test_data['age'] > 0, np.log1p(test_data['age']), test_age_min)
-
-	train_data = train_data.drop(columns=['area_m2', 'floor', 'age'])
-	test_data = test_data.drop(columns=['area_m2', 'floor', 'age'])'''
-
-	'''# K-Means Clustering
-	kmeans = KMeans(n_clusters=100, random_state=42)
-	train_data['region_cluster'] = kmeans.fit_predict(train_data[['latitude', 'longitude']])
-	test_data['region_cluster'] = kmeans.predict(test_data[['latitude', 'longitude']])'''
-
-	'''# Rolling Average
-	train_data['interest_rate_3mo_avg'] = train_data['interest_rate'].rolling(window=3).mean()
-	test_data['interest_rate_3mo_avg'] = test_data['interest_rate'].rolling(window=3).mean()
-	###'''
-
-	# weight 설정
-	# [x] TODO: Epoch time 적용
-	weight = 2
-	print(f'Setting weight to {weight}')
-	train_data = set_weight(train_data, weight)
-
+	print('start clustering')
 	# 필요한 열만 선택
 	train_data_clustering = train_data[['latitude', 'longitude', 'deposit']]
 	test_data_clustering = test_data[['latitude', 'longitude']]
@@ -72,29 +27,53 @@ def feature_engineering(train_data, test_data, interest_rate, subway_info, eleme
 	scaler = StandardScaler()
 	train_data_clustering_scaled = scaler.fit_transform(train_data_clustering)
 	test_data_clustering_scaled = scaler.transform(test_data_clustering)
-
 	# test_data_clustering_scaled에서 deposit에 해당하는 열 제거
 	test_data_clustering_scaled = test_data_clustering_scaled[:, :-1]
 	
 	# KMeans 클러스터링 수행
 	kmeans= KMeans(n_clusters=1000, init='k-means++', random_state=42)
-	train_data['region_cluster'] = kmeans.fit_predict(train_data_clustering_scaled)
+	train_data.loc[:, 'region_cluster'] = kmeans.fit_predict(train_data_clustering_scaled)
 
-	# 클러스터 중심 계산
-	centroids = train_data.groupby('region_cluster')[['latitude', 'longitude']].mean()
+	# vector similarity 계산을 위한 열
+	vector_similarity_columns = ['area_m2', 'contract_date_epoch', 'contract_type', 'floor', 'built_year', 'latitude', 'longitude','age', 'interest_rate']
 
-	# NearestCentroid를 사용하여 test_data에 클러스터 할당
-	nearest_centroid = NearestCentroid()
-	nearest_centroid.fit(centroids, centroids.index)
-	test_data['region_cluster'] = nearest_centroid.predict(test_data[['latitude', 'longitude']])
+	scaler = StandardScaler()
+	train_data_vector_scaled = scaler.fit_transform(train_data[vector_similarity_columns])
+	test_data_vector_scaled = scaler.transform(test_data[vector_similarity_columns])
 
-	columns_needed = ['deposit', 'area_m2', 'contract_date_epoch', 'floor', 'latitude', 'longitude',
+	train_data_vector_scaled = pd.DataFrame(train_data_vector_scaled, columns=vector_similarity_columns)
+	test_data_vector_scaled = pd.DataFrame(test_data_vector_scaled, columns=vector_similarity_columns)
+
+	train_data_vector_scaled.loc[:, 'region_cluster'] = train_data['region_cluster']
+
+	# cluster 중심 계산
+	centroids = train_data_vector_scaled.groupby('region_cluster').mean()
+
+	# cluster 중심과의 거리 계산
+	nc = NearestCentroid()
+	nc.fit(centroids, centroids.index)
+	test_data.loc[:, 'region_cluster'] = nc.predict(test_data_vector_scaled)
+	train_data.loc[:, 'region_cluster'] = nc.predict(train_data_vector_scaled.drop('region_cluster', axis=1))
+
+	facilities_info = {'subway': subway_info, 'elementary': elementary_info, 'middle': middle_info, 'high': high_info, 'park': park_info}
+	print('start train_data distance calculation')
+	train_data = calculate_nearst_distances_count_id(train_data, facilities_info)
+	print('start test_data distance calculation')
+	test_data = calculate_nearst_distances_count_id(test_data, facilities_info)
+
+	# weight 설정
+	# [x] TODO: Epoch time 적용
+	weight = 2
+	print(f'Setting weight to {weight}')
+	train_data = set_weight(train_data, weight)
+
+	columns_needed = ['deposit', 'area_m2', 'contract_date_epoch', 'contract_type', 'floor', 'built_year', 'latitude', 'longitude',
 						'subway_distance', 'elementary_distance', 'middle_distance', 'high_distance', 'park_distance', 
 						'subway_ID', 'elementary_ID', 'middle_ID', 'high_ID', 'park_ID',
 						'subway_count', 'elementary_count', 'middle_count', 'high_count', 'park_count',
 						'age', 'interest_rate', 'region_cluster', 'weight']
 
-	columns_needed_test = ['area_m2', 'contract_date_epoch', 'floor', 'latitude', 'longitude',
+	columns_needed_test = ['area_m2', 'contract_date_epoch', 'contract_type', 'floor', 'built_year', 'latitude', 'longitude',
 						'subway_distance', 'elementary_distance', 'middle_distance', 'high_distance', 'park_distance', 
 						'subway_ID', 'elementary_ID', 'middle_ID', 'high_ID', 'park_ID',
 						'subway_count', 'elementary_count', 'middle_count', 'high_count', 'park_count',
